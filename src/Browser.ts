dee0ef9f4a0f5ac4cb949d9edbb414d182d7e7d4
Pierre-Jean Lefort <pjgame84@gmail.com>
2022-04-19 19:36:22 +0200

    base for further operations

--- src/Browser.ts
+++ src/Browser.ts
@@ -1,13 +1,21 @@
 import * as puppeteer from "puppeteer-core";
-import * as fs from "fs";
-import * as os from "os";
 
 export default class Browser {
 	static browser: puppeteer.Browser;
 	static page: puppeteer.Page;
 
-	private static async getChromePath() {
-		return `${os.homedir()}\\AppData\\Local\\Google\\Chrome\\User Data`;
+	private static async fetchApi(
+		endpoint: string
+	): Promise<{ [key: string]: object } | null> {
+		if (!Browser.browser) return null;
+
+		const page = await Browser.browser.newPage();
+		page.goto(`${process.env.APP_URL}/api${endpoint}`);
+		await page.waitForNavigation();
+		const content = await page.evaluate(() => document.body.innerText);
+		await page.close();
+
+		return JSON.parse(content);
 	}
 
 	static async launchBrowser() {
@@ -16,28 +24,18 @@ export default class Browser {
 		}
 
 		try {
-			const extensions = [];
-			for (const extension of fs.readdirSync(
-				`${await Browser.getChromePath()}\\Default\\Extensions`
-			)) {
-				extensions.push(
-					`--load-extension=${await Browser.getChromePath()}\\Default\\Extensions\\${extension}`
-				);
-			}
-
-			Browser.browser = await puppeteer.launch({
+			const options: Parameters<typeof puppeteer.launch>[0] = {
 				headless: false,
+				devtools: false,
 				defaultViewport: null,
 				executablePath:
 					"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
 				ignoreDefaultArgs: ["--disable-extensions"],
-				args: [
-					`--user-data-dir=${await Browser.getChromePath()}`,
-					// ...extensions,
-				],
-			});
+			};
+
+			Browser.browser = await puppeteer.launch(options);
 		} catch (e) {
-			console.log("Browser opening error: ", e.message);
+			console.log("Browser opening error: ", e);
 			throw new Error(
 				"Please close all other instances of Chrome and try again."
 			);
--- src/electron.ts
+++ src/electron.ts
@@ -1,4 +1,6 @@
 import { BrowserWindow, app } from "electron";
+import * as dotenv from "dotenv";
 import Main from "./Main";
 
+dotenv.config();
 Main.main(app, BrowserWindow);
